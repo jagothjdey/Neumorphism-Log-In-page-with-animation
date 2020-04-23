a83d6375c290e0b14e2a52ddbfa9eeb3254e2951
dey <jagothjyotidey91@gmail.com>
2020-04-23 19:49:38 +0200

    Initial Commit

---
+++ RandomListProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RandomListProject
+//
+//  Created by Jagoth Jyoti Dey on 23.04.20.
+//  Copyright © 2020 Jagoth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
